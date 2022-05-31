2637c4cb3f6032576a23dad10dea4fafa1283ed7
UJ15 <dbwogml15@naver.com>
2022-05-31 18:38:08 +0900

    :sparkles: feat: user, post Repository

---
+++ board/src/main/java/com/uj15/board/entity/post/PostRepository.java
@@ -0,0 +1,7 @@
+package com.uj15.board.entity.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
---
+++ board/src/main/java/com/uj15/board/entity/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.uj15.board.entity.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
